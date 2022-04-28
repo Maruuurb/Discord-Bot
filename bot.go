24465583ea7caf86daae52f2497590f8277856b8
Mary <marurb557@gmail.com>
2022-04-28 19:17:29 +0300

    I add / commands

--- bot.go
+++ bot.go
@@ -13,82 +13,75 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
-// Variables used for command line parameters
-
 var (
-	Token string
+	GuildID        = flag.String("guild", "", "Test guild ID. If not passed - bot registers commands globally")
+	Token          string
+	RemoveCommands = flag.Bool("rmcmd", false, "Remove all commands after shutdowning or not")
 )
+var s *discordgo.Session
 
 func init() {
 	flag.StringVar(&Token, "t", "", "Bot Token")
 	flag.Parse()
 }
 
+var (
+	integerOptionMinValue = 1.0
+
+	commands = []*discordgo.ApplicationCommand{
+		{
+			Name: "ip",
+			// All commands and options must have a description
+			// Commands/options without description will fail the registration
+			// of the command.
+			Description: "The command that sends the server IP",
+		},
+	}
+
+	commandHandlers = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
+		"ip": messageCreate,
+	}
+)
+
 func main() {
-	// Create a new Discord session using the provided bot token.
-	dg, err := discordgo.New("Bot " + Token)
+	s, err := discordgo.New("Bot " + Token)
 	if err != nil {
 		fmt.Println("error creating Discord session,", err)
 		return
 	}
 
-	// Register the messageCreate func as a callback for MessageCreate events.
-	dg.AddHandler(messageCreate)
+	s.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		if h, ok := commandHandlers[i.ApplicationCommandData().Name]; ok {
+			h(s, i)
+		}
+	})
 
-	// Just like the ping pong example, we only care about receiving message
-	// events in this example.
-	dg.Identify.Intents = discordgo.IntentsGuildMessages
-
-	// Open a websocket connection to Discord and begin listening.
-	err = dg.Open()
+	s.Identify.Intents = discordgo.IntentsGuildMessages
+	err = s.Open()
 	if err != nil {
 		fmt.Println("error opening connection,", err)
 		return
 	}
+	log.Println("Adding commands...")
+	registeredCommands := make([]*discordgo.ApplicationCommand, len(commands))
+	for i, v := range commands {
+		cmd, err := s.ApplicationCommandCreate(s.State.User.ID, *GuildID, v)
+		if err != nil {
+			log.Panicf("Cannot create '%v' command: %v", v.Name, err)
+		}
+		registeredCommands[i] = cmd
+	}
 
-	// Wait here until CTRL-C or other term signal is received.
 	fmt.Println("Bot is now running. Press CTRL-C to exit.")
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
 	<-sc
 
-	// Cleanly close down the Discord session.
-	dg.Close()
+	s.Close()
 }
 
-// This function will be called (due to AddHandler above) every time a new
-// message is created on any channel that the authenticated bot has access to.
-//
-// It is called whenever a message is created but only when it's sent through a
-// server as we did not request IntentsDirectMessages.
-func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-	// Ignore all messages created by the bot itself
-	// This isn't required in this specific example but it's a good practice.
-	if m.Author.ID == s.State.User.ID {
-		return
-	}
-	// In this example, we only care about messages that are "ping".
-	if m.Content != "ip" {
-		return
-	}
+func messageCreate(s *discordgo.Session, m *discordgo.InteractionCreate) {
 
-	// We create the private channel with the user who sent the message.
-	_, err := s.UserChannelCreate(m.Author.ID)
-	if err != nil {
-		// If an error occurred, we failed to create the channel.
-		//
-		// Some common causes are:
-		// 1. We don't share a server with the user (not possible here).
-		// 2. We opened enough DM channels quickly enough for Discord to
-		//    label us as abusing the endpoint, blocking us from opening
-		//    new ones.
-		fmt.Println("error creating channel:", err)
-		s.ChannelMessageSend(
-			m.ChannelID,
-			"Something went wrong while sending the DM!",
-		)
-		return
-	}
 	resp, err := http.Get("http://myexternalip.com/raw")
 	if err != nil {
 		os.Stderr.WriteString(err.Error())
@@ -103,7 +96,18 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 			log.Fatal(err)
 		}
 		bodyString := string(bodyBytes)
-		_, err = s.ChannelMessageSend(m.ChannelID, "IP майнкрафт сервера : "+bodyString+":25565")
+		s.InteractionRespond(m.Interaction, &discordgo.InteractionResponse{
+
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+
+			Data: &discordgo.InteractionResponseData{
+
+				Flags: 1 << 6,
+
+				Content: "IP майнкрафт сервера : " + bodyString + ":25565",
+			},
+		})
+		//_, err = m.InteractionRespond(m.Interaction, "IP майнкрафт сервера : "+bodyString+":25565")
 		if err != nil {
 			// If an error occurred, we failed to send the message.
 			//
